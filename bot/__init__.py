b131592112333b341360eb7b18c12c5f0ea80765
Yohan Rockstar <77877340+yohanrock@users.noreply.github.com>
2025-10-22 10:57:11 +0530

    Remove uvloop installation and clean up imports

--- bot/__init__.py
+++ bot/__init__.py
@@ -1,9 +1,6 @@
 from time import time
 from logging import getLogger, FileHandler, StreamHandler, INFO, ERROR, basicConfig
 
-from uvloop import install
-
-install()
 basicConfig(format="[%(asctime)s] [%(levelname)s] - %(message)s",
             datefmt="%d-%b-%y %I:%M:%S %p",
             handlers=[FileHandler('log.txt'), StreamHandler()],
@@ -17,4 +14,3 @@ LOGGER = getLogger(__name__)
 StartTime = time()
 
 __version__ = "1.2.6"
-
